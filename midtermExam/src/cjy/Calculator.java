6ad277320391aa464fd5cd69d85b655f63cef947
miu02 <miu02@192.168.219.101>
2024-10-20 22:29:56 +0900

    외형 수정 2

--- midtermExam/src/cjy/Calculator.java
+++ midtermExam/src/cjy/Calculator.java
@@ -1,17 +1,40 @@
 package cjy;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
+/**
+ * 간단한 윈도우 계산기입니다.
+ * 
+ * @author Choi Jong Yun 
+ * @version 1.1
+ * @since 1.0
+ * 
+ * @created 2024-10-19
+ * @lastModified 2024-10-20
+ * 
+ * @changelog
+ * <ul>
+ *   <li>2024-10-19: 최초 생성</li>
+ *   <li>2024-10-20: 계산기 외형 변경</li>
+ * </ul>
+ */
+
 public class Calculator extends JFrame {
+	JTextField t1;
+	double num1 = 0, num2 = 0;
+	String opr = "";
+	
 	Calculator() {
 		setTitle("계산기");
-        setSize(400, 600);
+        setSize(300, 500);
         setLayout(new BorderLayout());
         
-		JTextField t1 = new JTextField("0");
-		t1.setFont(new Font("Arial", Font.PLAIN, 30));
+		t1 = new JTextField("0");
+		t1.setFont(new Font("Arial", Font.PLAIN, 30)); // 결과 창 폰트 변경 @see chatGPT
 		t1.setHorizontalAlignment(JTextField.RIGHT);
+		t1.setPreferredSize(new Dimension(300, 100)); // 결과 창 크기 조정 @see chatGPT
 		t1.setEnabled(false);
 		add(t1, "North");
 
